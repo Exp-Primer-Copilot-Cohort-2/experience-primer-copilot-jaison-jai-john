681dafd3f3b799dd1f253b7c79dca97d05435d36
Jaison <76701333+jaison-jai-john@users.noreply.github.com>
2024-07-07 03:08:53 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,58 +1,60 @@
 // Create web server
-// npm install express
-// npm install body-parser
-// npm install mysql
-// npm install cors
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const cors = require('cors');
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-const connection = mysql.createConnection({
+// Create web server
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var mysql = require('mysql');
+var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'admin',
     database: 'comment'
 });
+connection.connect();
 
-connection.connect((err) => {
-    if (err) {
-        console.log('Error connecting to database');
-        return;
-    }
-    console.log('Connection to database established');
-});
+app.use(express.static('public'));
 
-app.get('/comments', (req, res) => {
-    const sql = 'SELECT * FROM comments';
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.log('Error retrieving comments');
-            res.status(500).json({ error: 'Error retrieving comments' });
-            return;
-        }
-        res.json(result);
+// Get all comments
+app.get('/getComments', function(req, res) {
+    connection.query('SELECT * FROM comments', function(err, rows, fields) {
+        if (err) throw err;
+        res.send(rows);
     });
 });
 
-app.post('/comments', (req, res) => {
-    const comment = req.body.comment;
-    const sql = 'INSERT INTO comments (comment) VALUES (?)';
-    connection.query(sql, [comment], (err, result) => {
-        if (err) {
-            console.log('Error inserting comment');
-            res.status(500).json({ error: 'Error inserting comment' });
-            return;
-        }
-        res.status(201).json({ message: 'Comment inserted' });
+// Post a comment
+app.post('/postComment', urlencodedParser, function(req, res) {
+    var comment = req.body.comment;
+    var user = req.body.user;
+    connection.query('INSERT INTO comments (user, comment) VALUES (?, ?)', [user, comment], function(err, result) {
+        if (err) throw err;
+        res.send('Comment posted successfully!');
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
+// Update a comment
+app.post('/updateComment', urlencodedParser, function(req, res) {
+    var id = req.body.id;
+    var comment = req.body.comment;
+    connection.query('UPDATE comments SET comment = ? WHERE id = ?', [comment, id], function(err, result) {
+        if (err) throw err;
+        res.send('Comment updated successfully!');
+    });
+});
+
+// Delete a comment
+app.post('/deleteComment', urlencodedParser, function(req, res) {
+    var id = req.body.id;
+    connection.query('DELETE FROM comments WHERE id = ?', [id], function(err, result) {
+        if (err) throw err;
+        res.send('Comment deleted successfully!');
+    });
+});
+
+var server = app.listen(8081, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server listening at http://%s:%s', host, port);
 });
